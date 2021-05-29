db10121d760b90ae90cd3c1cffd300cdaf455fd9
Xenia Büscher <xeniaainex@web.de>
2021-05-29 10:42:30 +0200

    Sprint 1: BLI1: Setup Webservice

---
+++ Entwicklung/SE/src/main/java/SE3_Projekt/SE/SeWwi18B3Application.java
@@ -0,0 +1,13 @@
+package SE3_Projekt.SE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeWwi18B3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeWwi18B3Application.class, args);
+	}
+
+}
---
+++ Entwicklung/SE/src/test/java/SE3_Projekt/SE/SeWwi18B3ApplicationTests.java
@@ -0,0 +1,13 @@
+package SE3_Projekt.SE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeWwi18B3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
