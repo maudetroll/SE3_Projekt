7eff57636b25dde2d1b0e6e7ca70dc638495a979
Johannes Wessiepe <j.wessiepe@gmail.com>
2021-06-19 12:28:10 +0200

    Sprint 3: Refactor ModellEntry.java

--- Entwicklung/SE/src/main/java/SE3_Projekt/SE/ModellEntry.java
+++ Entwicklung/SE/src/main/java/SE3_Projekt/SE/ModellEntry.java
@@ -16,31 +16,7 @@ import lombok.Data;
 @Entity
 @Data
 public class ModellEntry implements Serializable {
-	public String getModellName() {
-		return modellName;
-	}
-	public void setModellName(String modellName) {
-		this.modellName = modellName;
-	}
-	public double getDisplayGroeße() {
-		return displayGroeße;
-	}
-	public void setDisplayGroeße(double displayGroeße) {
-		this.displayGroeße = displayGroeße;
-	}
-	public String getProzessorName() {
-		return prozessorName;
-	}
-	public void setProzessorName(String prozessorName) {
-		this.prozessorName = prozessorName;
-	}
-
-	public double getPreis() {
-		return preis;
-	}
-	public void setPreis(double preis) {
-		this.preis = preis;
-	}
+	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
@@ -51,24 +27,53 @@ public class ModellEntry implements Serializable {
     private MobilTelHerstellerEntry mobilTelHersteller;
 	
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<L�nge>=64")
+    @Size(min = 1, max = 64, message = "1<L�nge>=64")
     @NotNull(message = "Muss ungleich null sein")
 	private String modellName;
     
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<L�nge>=64")
     @NotNull(message = "Muss ungleich null sein")
-	private double displayGroeße;
+	private double displayGroesse;
     
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<L�nge>=64")
+    @Size(min = 1, max = 64, message = "1<L�nge>=64")
     @NotNull(message = "Muss ungleich null sein")
 	private String prozessorName;
     
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<L�nge>=64")
     @NotNull(message = "Muss ungleich null sein")
 	private double preis;
     
-    
+	public String getModellName() {
+		return modellName;
+	}
+	
+	public void setModellName(String modellName) {
+		this.modellName = modellName;
+	}
+	
+	public double getDisplayGroesse() {
+		return displayGroesse;
+	}
+	
+	public void setDisplayGroesse(double displayGroesse) {
+		this.displayGroesse = displayGroesse;
+	}
+	
+	public String getProzessorName() {
+		return prozessorName;
+	}
+	
+	public void setProzessorName(String prozessorName) {
+		this.prozessorName = prozessorName;
+	}
+	
+
+	public double getPreis() {
+		return preis;
+	}
+	
+	public void setPreis(double preis) {
+		this.preis = preis;
+	}
 }
