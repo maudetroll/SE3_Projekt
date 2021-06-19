d5f15f63890443f15bb4ea2e14633ccf1aee4b01
Johannes Wessiepe <j.wessiepe@gmail.com>
2021-06-19 12:34:53 +0200

    Sprint 3: Refactor MobilTelHersteller.java

--- Entwicklung/SE/src/main/java/SE3_Projekt/SE/MobilTelHerstellerEntry.java
+++ Entwicklung/SE/src/main/java/SE3_Projekt/SE/MobilTelHerstellerEntry.java
@@ -18,60 +18,67 @@ import lombok.Data;
 @Entity
 @Data
 public class MobilTelHerstellerEntry implements Serializable {
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getGruendungsjahr() {
-		return gruendungsjahr;
-	}
-	public void setGruendungsjahr(int gruendungsjahr) {
-		this.gruendungsjahr = gruendungsjahr;
-	}
-	public int getMitarbeiteranzahl() {
-		return mitarbeiteranzahl;
-	}
-	public void setMitarbeiteranzahl(int mitarbeiteranzahl) {
-		this.mitarbeiteranzahl = mitarbeiteranzahl;
-	}
-	public int getUmsatz() {
-		return umsatz;
-	}
-	public void setUmsatz(int umsatz) {
-		this.umsatz = umsatz;
-	}
-    @ManyToOne
-    private HerkunftslandEntry herkunftsland;
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     private long id;
+	
+	@ManyToOne
+    private HerkunftslandEntry herkunftsland;
     
     @Column(length = 64)
-    @Size(min = 1, max = 64, message = "1<Länge>=64")
+    @Size(min = 1, max = 64, message = "1<Lï¿½nge>=64")
     @NotNull(message = "Muss ungleich null sein")
     private String name = "";
     
     
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<Länge>=64")
     @NotNull(message = "Muss ungleich null sein")
 	private int gruendungsjahr;
     
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<Länge>=64")
     @NotNull(message = "Muss ungleich null sein")
 	private int mitarbeiteranzahl;
     
     @Column(length = 64)
-    //@Size(min = 1, max = 64, message = "1<Länge>=64")
     @NotNull(message = "Muss ungleich null sein")
 	private int umsatz;
     
     @OneToMany(mappedBy="mobilTelHersteller")
     private List<ModellEntry> modelle = new ArrayList<>();
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getGruendungsjahr() {
+		return gruendungsjahr;
+	}
+	
+	public void setGruendungsjahr(int gruendungsjahr) {
+		this.gruendungsjahr = gruendungsjahr;
+	}
+	
+	public int getMitarbeiteranzahl() {
+		return mitarbeiteranzahl;
+	}
+	
+	public void setMitarbeiteranzahl(int mitarbeiteranzahl) {
+		this.mitarbeiteranzahl = mitarbeiteranzahl;
+	}
+	
+	public int getUmsatz() {
+		return umsatz;
+	}
+	
+	public void setUmsatz(int umsatz) {
+		this.umsatz = umsatz;
+	}
 
 }
