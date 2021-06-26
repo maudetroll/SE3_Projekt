f9c8b8e75b2b6664433fdc5248d244dffc2de10d
maudetroll <55143852+maudetroll@users.noreply.github.com>
2021-06-26 12:16:54 +0200

    BLI 6: Added URLs for pictures

--- Entwicklung/SE/src/main/resources/data.sql
+++ Entwicklung/SE/src/main/resources/data.sql
@@ -1,9 +1,6 @@
-
 INSERT INTO HERKUNFTSLAND_ENTRY (name,einwohnerzahl,gruendungsjahr) VALUES ('Volksrepublik China',1300000000, 1949);
 INSERT INTO HERKUNFTSLAND_ENTRY (name,einwohnerzahl,gruendungsjahr) VALUES ('Sued-Korea',51470000,1945);
 INSERT INTO HERKUNFTSLAND_ENTRY (name,einwohnerzahl,gruendungsjahr) VALUES ('USA',327200000,1776);
-
-
 
 INSERT INTO MOBIL_TEL_HERSTELLER_ENTRY   (HERKUNFTSLAND_ID,name,gruendungsjahr,mitarbeiteranzahl,umsatz) VALUES (2,'Samsung',1938,309630,21120000);
 INSERT INTO MOBIL_TEL_HERSTELLER_ENTRY   (HERKUNFTSLAND_ID,name,gruendungsjahr,mitarbeiteranzahl,umsatz) VALUES (1,'Huawei',1987,194000,122000000);
@@ -14,20 +11,21 @@ INSERT INTO MOBIL_TEL_HERSTELLER_ENTRY   (HERKUNFTSLAND_ID,name,gruendungsjahr,m
 
 
 
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (1,'S10',6.1,'Exynos 9820',899);
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (1,'S20',6.2,'Exynos 990',899);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (1,'S10',6.1,'Exynos 9820','https://images.samsung.com/is/image/samsung/assets/de/smartphones/galaxy-s10/sunset/images/gallery/galaxy-s10-plus_gallery-color_s10-plus-c1-01.jpg?$ORIGIN_JPG$',899);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (1,'S20',6.2,'Exynos 990','https://images-na.ssl-images-amazon.com/images/I/71VUs8CuQ5L._AC_SX450_.jpg',899);
 
 
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (2,'P40 Pro',6.58,'Kirin 990',970);
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (2,'P30 Pro',6.0,'HiSilicon Kirin 980',506);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (2,'P40 Pro',6.58,'Kirin 990','https://www.meintrendyhandy.de/images/Huawei-P40-Pro-256GB-Midnight-Black-6901443378944-29042020-01-p.jpg',970);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (2,'P30 Pro',6.0,'HiSilicon Kirin 980','https://images-na.ssl-images-amazon.com/images/I/51KWBBapwQL._AC_SX679_.jpg',506);
 
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (3,'11',6.1,'Apple A13 Bionic',799);
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (3,'10',5.8,'Apple A11',620);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (3,'12',6.1,'Apple A13 Bionic','https://images-na.ssl-images-amazon.com/images/I/71IluxZN8pL._AC_SX522_.jpg',799);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (3,'11',5.8,'Apple A11','https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-select-2019-family_GEO_EMEA?wid=882&hei=1058&fmt=jpeg&qlt=80&.v=1567022219953',620);
 
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (4,'MI Note 10 Pro',6.47,'Snapdragon 855',500);
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (4,'MI Note 9 Pro',6.4,'Snapdragon 855',375);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (4,'MI Note 10 Pro',6.47,'Snapdragon 855','https://images-na.ssl-images-amazon.com/images/I/41DnIsw5sLL._AC_SX425_.jpg',500);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (4,'MI Note 9 Pro',6.4,'Snapdragon 855','https://images-na.ssl-images-amazon.com/images/I/61-VjRugPeL._AC_SX679_.jpg',375);
 
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (5,'Find X2 Pro',6.78,'Snapdragon 865',1200);
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (5,'F11 Pro',6.53,'MediaTek Helio P70',250);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (5,'Find X2 Pro',6.78,'Snapdragon 865','https://images-na.ssl-images-amazon.com/images/I/91CTz0Y4wGL._AC_SX522_.jpg',1200);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (5,'F11 Pro',6.53,'MediaTek Helio P70','https://manual-user-guide.com/images/phones/oppo_f11_pro.png',250);
 
-INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,preis) VALUES (6,'8 T',6.55,'Snapdragon 865',470);
+INSERT INTO MODELL_ENTRY (MOBIL_TEL_HERSTELLER_ID,modell_name,display_groesse,prozessor_name,url_bild,preis) VALUES (6,'8 T',6.55,'Snapdragon 865','https://oasis.opstatics.com/content/dam/oasis/default/product-specs/8t-lunar.png',470);
+
